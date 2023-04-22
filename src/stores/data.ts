672b182847f641206dc1c0907db3f7b80e5148be
Renzo Castillo <renzo.castillo@quaira.com>
2023-04-22 12:32:09 -0500

    jsdoc blocks for pinia stores

--- src/stores/data.ts
+++ src/stores/data.ts
@@ -4,13 +4,30 @@ import {apiData, apiName, apiURL, nonce, translationStrings} from "@/router";
 import axios from 'axios';
 import {toDate} from "@/helper";
 
+/**
+ * Represents the Graph Item
+ *
+ * @property {number} date - The graph item date
+ * @property {number} value - The graph item value
+ */
 type GraphItem = {
     date: number,
     value: number,
 }
 
+/**
+ * Represents a graph as an array of graph items.
+ */
 type Graph = Array<GraphItem>;
 
+/**
+ * Represents a table with a title and data.
+ *
+ * @property {string} title - The title of the table.
+ * @property {object} data - The data of the table.
+ * @property {string[]} data.headers - The headers of the table.
+ * @property {object[]} data.rows - The rows of the table.
+ */
 type Table = {
     title: string,
     data : {
@@ -24,14 +41,32 @@ type Table = {
         }>
     }
 }
+
+/**
+ * Represents a response containing remote data, including a graph and a table.
+ *
+ * @property {Graph} graph - The graph data.
+ * @property {Table} table - The table data.
+ */
 type RemoteDataResponse = {
     graph: Graph,
     table: Table
 }
 
+/**
+ * Pinia store variable which contain all the data retrieving through data API
+ */
 export const useDataStore = defineStore('data',()=>{
     const graph = ref(<Graph>{});
     const table = ref(<Table>{});
+    /**
+     * Fetches data from a remote API and updates state variables with the response data.
+     *
+     * @async
+     * @param {boolean} [update=false] - Whether to update the API data or not.
+     * @throws {Error} If the API request fails.
+     * @returns {Promise<void>}
+     */
     const callData = async (update:boolean = false) => {
         const apiRemoteData = apiURL + apiName + apiData + "?update="+update
         const config = {
@@ -49,12 +84,29 @@ export const useDataStore = defineStore('data',()=>{
         table.value = tableData;
     };
 
+    /**
+     * Extracts data from the `graph` state variable and formats it for use in a chart.
+     *
+     * @returns {ChartData} An object containing labels and datasets that can be used to display a chart.
+     */
     const  chartData = ()=>{
+        /**
+         * Extracts data from the `graph` state variable for use in a chart.
+         *
+         * @type {Graph}
+         */
         const graphData:Graph = graph.value;
+
+        /**
+         * Generates an array of date strings from the `graphData` constant.
+         */
         const dates= Object.keys(graphData).map((index)=>{
             const item= graphData[parseInt(index)]
             return toDate(item.date).toLocaleString();
         });
+        /**
+         * Generates an array of data values from the `graphData` constant.
+         */
         const values= Object.keys(graphData).map((index)=>{
             const item= graphData[parseInt(index)]
             return item.value;
--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -3,27 +3,63 @@ import {defineStore} from "pinia";
 import {ref} from "vue";
 import {apiName, apiSettings, apiURL, nonce} from "@/router";
 
+/**
+ * Represents the response data for settings.
+ *
+ * @property {string} numrows - The rows table number setting
+ * @property {boolean} humandate - The human date setting for the dates at the table view
+ * @property {string[]} emails - The set of emails that are retrieved at the list component view
+ */
 type SettingsDataResponse ={
     numrows: string,
     humandate: boolean,
     emails: Array<string>,
 }
 
+/**
+ * Pinia store variable which contains all the state variable for the settings api
+ */
 export const useSettingsStore = defineStore('setter',()=>{
+    /**
+     * The set of emails that are retrieved at the list component view
+     */
     const emails = ref<string []>([]);
+    /**
+     * The human date setting for the dates at the table view
+     */
     const humandate = ref(true);
+    /**
+     * The rows table number setting
+     */
     const numrows = ref(0);
+    /**
+     * Saves the error response generated when making the callSettings api request
+     */
     let callSettingsError: Error = {
         name: "",
         message: ""
     }
+    /**
+     * Saves the error response generated when making the updateSettings api request
+     */
     let updateSettingError: Error = {
         name: "",
         message: ""
     }
 
+    /**
+     * Retrieve all settings through API
+     *
+     * @return Data object containing all settings values
+     */
     const callSettings = async () => {
+        /**
+         * The API url from which settings are going to be retrieved
+         */
         const apiGetSettings = apiURL + apiName + apiSettings;
+        /**
+         * The config parameters for the Axios request, which incldues the X-WP-Nonce header for authentication
+         */
         const config = {
             headers: {
                 'X-WP-Nonce': nonce,
@@ -58,17 +94,26 @@ export const useSettingsStore = defineStore('setter',()=>{
         }
     }
 
+    /**
+     * Updates a specific setting by making an Axios post request to the settings API
+     * @param key The option key identifier
+     * @param value The value that will be updated to the key option.
+     */
     const updateSetting = async (key:string, value:any)=> {
-        /*console.log("key is");
-        console.log(key);
-        console.log("value is");
-        console.log(value);
-        console.log(nonce);*/
+        /**
+         * The API url which will be used to update our settings
+         */
         const apiUpdateSettings = apiURL + apiName + apiSettings;
+        /**
+         * The data object to be sent at the post request which contains the option key and option value
+         */
         const data = {
             key: key,
             value: value,
         }
+        /**
+         * The config parameters for the Axios request, which incldues the X-WP-Nonce header for authentication
+         */
         const config = {
             headers: {
                 'X-WP-Nonce': nonce,
